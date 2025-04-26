d41f21a2faaa85ff9ac80bc285dc14e0e300fddd
未觉寻星 <2937150394@qq.com>
2025-04-27 05:30:13 +0800

    Site updated: 2025-04-27 05:30:10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1745697444818";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1745703009046";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
