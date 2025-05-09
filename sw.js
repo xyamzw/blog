f211bf8dfca21aba1723b290ed1f09714c337aa1
未觉寻星 <2937150394@qq.com>
2025-05-09 18:19:50 +0800

    Site updated: 2025-05-09 18:19:46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1746720565299";const preCache = ["/images/taichi.png","http://xyamzw.test.upcdn.net//images/blog/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1746785984538";const preCache = ["/images/taichi.png","https://cdn.jsdelivr.net/gh/xyamzw/xyamzw/images/blog/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
